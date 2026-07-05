12fa6ceba7ccfbeaf670b1f16dee115251e36eba
Shaikibbu07 <138100980+Shaikibbu07@users.noreply.github.com>
2026-07-05 23:55:35 +0530

    Initial commit

---
+++ my.js
@@ -0,0 +1,29 @@
+// Alert Box
+alert("Welcome to JavaScript!");
+
+// Print message in Console
+console.log("This message is logged in the console!");
+
+// Display text on webpage
+document.write("<h2>JavaScript !</h2>");
+// Variables
+let name = "Ibbu";
+let age = 21;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Display data in webpage
+let marks = 85;
+document.write("Name: " + name);
+document.write("<p>Age: " + age + "</p>");
+document.write("<p>Marks: " + marks + "</p>");
+if (marks >= 90) {
+    document.write("<p>Grade: A</p>");
+} else if (marks >= 80) {
+    document.write("<p>Grade: B</p>");
+} else if (marks >= 70) {
+    document.write("<p>Grade: C</p>");
+} else {
+    document.write("<p>Grade: D</p>");
+}
